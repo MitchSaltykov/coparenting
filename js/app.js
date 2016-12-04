22bb9a4933ac97dda182eb0e4771408767ba6259
Mike Gertrudes <mikegertrudes@gmail.com>
2016-12-03 21:40:23 -0500

    tightening up

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+window.APP = {};
+
+APP.showOverlay = function() {
+	var overlay = document.getElementById('overlay');
+
+	var overlayChild = overlay.querySelector('.overlay-child');
+
+	overlay.style.display = 'block';
+
+	overlay.addEventListener('click', function(event) {
+		if (event.target === overlay) {
+			overlay.style.display = 'none';
+			event.target.removeEventListener('click', event, false);
+		}
+	});
+};
